63aa6b7b30ef133682050a577dec4cb39ef88d15
Mike Hardisty <michaelmrh@gmail.com>
2023-09-06 23:45:50 -0700

    Level 1.5: file server

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	corsMux := middlewareCors(mux)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: corsMux}
