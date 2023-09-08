1681e58fbd878c5884d3a1c93447ee9486d6da8a
Mike Hardisty <michaelmrh@gmail.com>
2023-09-07 21:40:50 -0700

    Lesson 1.11: Custom Handlers

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", myHandler)
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
@@ -26,4 +26,11 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
 }
